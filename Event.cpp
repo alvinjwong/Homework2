98f64c2fc0e51b286b7847cc82162ad3ec766be5
alvinjwong <ajwong@uab.edu>
2024-10-10 12:41:27 -0500

    update

--- Event.cpp
+++ Event.cpp
@@ -10,6 +10,8 @@ class Event {
     std::string eventDate;
     std::string location;
     public:
-    
+    Event(std::string name, std::string date, std::string location) : eventName(name), eventDate(date), location(location) {}
+    void getDetails();
+    bool isUpcoming();
 
-}
+};
