a959f1136828fd953343ad7eb6e963fe2bbe19e2
alvinjwong <ajwong@uab.edu>
2024-10-10 13:55:47 -0500

    workshop

--- Event.cpp
+++ Event.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <string>
+#include <ctime>
 
+//https://www.w3schools.com/cpp/cpp_date.asp#:~:text=The%20difftime()%20function%20measures,measuring%20time%20differences%20between%20dates.
 
 
 
@@ -11,7 +13,17 @@ class Event {
     std::string location;
     public:
     Event(std::string name, std::string date, std::string location) : eventName(name), eventDate(date), location(location) {}
-    void getDetails();
-    bool isUpcoming();
+    void getDetails() {std::cout << "Event: " << eventName << " Date: " << eventDate << " Location: " << location << std::endl;}
+    bool isUpcoming() {return true;}
+};
 
+class Workshop : public Event {
+    private:
+    int duration;
+    std::string instructor;
+    public:
+    Workshop(std::string name, std::string date, std::string location, int duration, std::string instructor) :
+    Event(name, date, location), duration(duration), instructor(instructor) {}
+
+    
 };
